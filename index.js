9730e13dc11034d1f144bc709f8e02c8e4dbc47d
behruz <sultonmurotovbehruzbek2010>
2025-10-24 17:28:11 +0500

    TELEGRAMDA GA TEKEN BERDIM

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const TOKEN = "8005568684:AAFWXfVJvDJHBE7nZxDyWFWD5KNjiFqebfM"
 
 const bot =new TelegramBot(TOKEN,{ polling: true})
 
+bot.on("message", function(msg) {
+    const chatID = msg.chat.id;
+    bot.sendMessage(chatID,"XURMATLI MIZOR BIZNING BOTIMIZDA NIMA KERE")
+}
+)
